04d3d1ff675edd9d944f6f6a2fe40cac48fe5c66
Bobae Kim <rlaqhqo0418@naver.com>
2024-08-24 16:43:45 +0900

    feat: Wish 단일조회 기능 구현

--- src/main/java/com/example/goodnight3rdhackathonbackend/service/WishService.java
+++ src/main/java/com/example/goodnight3rdhackathonbackend/service/WishService.java
@@ -1,7 +1,11 @@
 package com.example.goodnight3rdhackathonbackend.service;
 
 import com.example.goodnight3rdhackathonbackend.domain.Wish;
+import com.example.goodnight3rdhackathonbackend.domain.WishConfirmState;
 import com.example.goodnight3rdhackathonbackend.dto.WishDto;
+import com.example.goodnight3rdhackathonbackend.error.ErrorCode;
+import com.example.goodnight3rdhackathonbackend.error.NotApprovedWishException;
+import com.example.goodnight3rdhackathonbackend.error.NotFoundWishException;
 import com.example.goodnight3rdhackathonbackend.repository.WishRepository;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +40,24 @@ public class WishService {
         Wish targetWish = wishRepository.findById(id);
         targetWish.setIs_confirm(wishDto.getIs_confirm());
         wishRepository.updateById(id, targetWish);
+    }
+
+    public WishDto.FindDto findWishById(Long id) {
+        Wish targetWish = wishRepository.findById(id);
+        if (targetWish.getDeleted_at() != null) {
+            throw new NotFoundWishException(ErrorCode.NOT_EXIST_WISH);
+        }
+        if (targetWish.getIs_confirm() != WishConfirmState.APPROVED.getKorean()) {
+            throw new NotApprovedWishException(ErrorCode.NOT_APPROVED_WISH);
+        }
+
+
+        WishDto.FindDto wishDto = new WishDto.FindDto();
+        wishDto.setTitle(targetWish.getTitle());
+        wishDto.setContent(targetWish.getContent());
+        wishDto.setCategory(targetWish.getCategory());
+
+        return wishDto;
     }
 
 }
