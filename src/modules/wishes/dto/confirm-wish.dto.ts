7dd11ebadb549e7f3ed63df0c3112c9a2bc6a3c6
dlwhsk0 <hana.dev.314@gmail.com>
2024-08-24 22:03:52 +0900

    feat: 카테고리, 승인 상태 분류 enum 설정

--- src/modules/wishes/dto/confirm-wish.dto.ts
+++ src/modules/wishes/dto/confirm-wish.dto.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+enum Confirm {
+    승인됨 = '승인됨',
+    보류됨 = '보류됨',
+    거절됨 = '거절됨',
+}
 
 export class ConfirmWishDto {
     @IsNotEmpty()
     readonly id: number;
 
     @IsNotEmpty()
+    @IsEnum(Object.values(Confirm))
     readonly isConfirm: string;
 }
--- src/modules/wishes/dto/create-wish.dto.ts
+++ src/modules/wishes/dto/create-wish.dto.ts
@@ -1,4 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+enum Category {
+    진로 = '진로',
+    건강 = '건강',
+    인간관계 = '인간 관계',
+    돈 = '돈',
+    목표 = '목표',
+    학업성적 = '학업/성적',
+    기타 = '기타',
+}
 
 export class CreateWishDto {
     @IsNotEmpty()
@@ -8,5 +17,6 @@ export class CreateWishDto {
     readonly content: string;
 
     @IsNotEmpty()
+    @IsEnum(Object.values(Category))
     readonly category: string;
 }
