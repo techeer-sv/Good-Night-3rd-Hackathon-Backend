ea4c446bfed904675f492f8b65c5a9ae714878cf
j._.jinoo <jinoo0306@naver.com>
2024-08-24 20:26:50 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/techeer/tree/Good_Night_3rd_Hackathon_Backend/TecheerTreeApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.tree.Good_Night_3rd_Hackathon_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheerTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeer/tree/Good_Night_3rd_Hackathon_Backend/TecheerTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.tree.Good_Night_3rd_Hackathon_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
