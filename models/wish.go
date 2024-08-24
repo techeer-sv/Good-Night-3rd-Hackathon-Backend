2b7395ab651612ca9685129e802624750c2b768b
Jaehoon <gkdwngo@gmail.com>
2024-08-25 03:17:57 +0900

    Wish 모델 정의

---
+++ models/wish.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Category string
+
+const (
+	Career    		Category = "진로"
+	Health   		  Category = "건강"
+	Relationships Category = "인간관계"
+	Money     		Category = "돈"
+	Goals      		Category = "목표"
+	Academics  		Category = "학업/성적"
+	Other      		Category = "기타"
+)
+
+type WishStatus string
+
+const (
+	Approved WishStatus = "approved"
+	Pending  WishStatus = "pending"
+	Rejected WishStatus = "rejected"
+)
+
+type Wish struct {
+	ID        int            `json:"id" gorm:"primaryKey"`
+	Title     string         `json:"title" validate:"required"`
+	Content   string         `json:"content" validate:"required"`
+	Category  Category       `json:"category" validate:"required"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	IsConfirm WishStatus     `json:"is_confirm" gorm:"default:'pending'"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
