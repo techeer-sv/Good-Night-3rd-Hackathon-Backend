23ea62c3718c8f21f77ca8d459def4c40b630614
dongmin <dongmin11566@gmail.com>
2024-08-24 15:19:49 +0900

    feat: wishes, comments 엔티티 생성

---
+++ src/comments/entities/comment.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Wish } from '../../wishes/entities/wish.entity'
+
+@Entity('comments')
+export class Comment {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    content: string;
+
+    @ManyToOne(() => Wish, wish => wish.comments)
+    wish: Wish;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column({ nullable: true })
+    deleted_at?: Date;
+}
---
+++ src/wishes/entities/wish.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, CreateDateColumn } from 'typeorm';
+import { Comment } from '../../comments/entities/comment.entity';
+
+@Entity('wishes')
+export class Wish {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    title: string;
+
+    @Column({ nullable: false })
+    content: string;
+
+    @Column({ nullable: false })
+    category: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column({ type: 'enum', enum: ['승인됨', '보류됨', '거절됨'], default: '보류됨' })
+    is_confirm: string;
+
+    @Column({ nullable: true })
+    deleted_at?: Date;
+
+    @OneToMany(() => Comment, comment => comment.wish)
+    comments: Comment[];
+}
