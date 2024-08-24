b517ba5659d394c91cd102ff71bc649a6c3779c8
dongmin <dongmin11566@gmail.com>
2024-08-24 15:37:42 +0900

    feat: 소원 생성

---
+++ src/wishes/wishes.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wish } from './entities/wish.entity';
+import { CreateWishDto } from './dtos/create-wish.dto';
+
+@Injectable()
+export class WishesService {
+  constructor(
+    @InjectRepository(Wish)
+    private readonly wishRepository: Repository<Wish>,
+  ) {}
+
+  async create(createWishDto: CreateWishDto): Promise<Wish> {
+    const wish = this.wishRepository.create(createWishDto);
+    return this.wishRepository.save(wish);
+  }
+}
