08ff08e9990d65cf7908a9acef3e4f806592f408
GiHwan2 <sandwich5011@naver.com>
2024-08-24 13:04:01 +0900

    chore: 초기 개발 환경 세팅

---
+++ src/main/java/com/example/Good_Night_3rd_Hackathon_Backend/GoodNight3rdHackathonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Good_Night_3rd_Hackathon_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodNight3rdHackathonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodNight3rdHackathonBackendApplication.class, args);
+	}
+
+}
