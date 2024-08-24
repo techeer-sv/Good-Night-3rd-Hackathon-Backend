51f357917ccea7a0f6eaff57e782b1a6926c0a23
d0kyoung <ehrud0522@gmail.com>
2024-08-24 23:56:48 +0900

    comment router 추가

--- server/router.go
+++ server/router.go
@@ -26,12 +26,21 @@ func setupRouter(handler *handlers.Handler) *gin.Engine {
 
 		wishGroup := apiGroup.Group("/wishes")
 		{
-			wishGroup.POST("/", handler.WishHandler.RegisterWish)          // 소원 등록 (Create)
-			wishGroup.GET("/:id", handler.WishHandler.GetWish)             // 소원 단일 조회 (Read)
-			wishGroup.GET("/", handler.WishHandler.GetAllWishes)           // 소원 목록 조회 (Read)
-			wishGroup.PUT("/:id/approve", handler.WishHandler.ApproveWish) // 소원 승인 (Update)
-			wishGroup.PUT("/:id/reject", handler.WishHandler.RejectWish)   // 소원 거절 (Update)
-			wishGroup.DELETE("/:id", handler.WishHandler.DeleteWish)       // 소원 삭제 (Delete)
+			wishGroup.POST("/", handler.WishHandler.RegisterWish)            // 소원 등록
+			wishGroup.GET("/:id", handler.WishHandler.GetWish)               // 소원 단일 조회
+			wishGroup.GET("/", handler.WishHandler.GetAllWishes)             // 소원 목록 조회
+			wishGroup.GET("/pending", handler.WishHandler.GetPendingWishes)  // 보류된 소원 조회
+			wishGroup.PATCH("/:id/approve", handler.WishHandler.ApproveWish) // 소원 승인
+			wishGroup.PATCH("/:id/reject", handler.WishHandler.RejectWish)   // 소원 거절
+			wishGroup.DELETE("/:id", handler.WishHandler.DeleteWish)         // 소원 삭제
+		}
+
+		commentGroup := apiGroup.Group("/comments")
+		{
+			commentGroup.POST("/", handler.CommentHandler.CreateComment)                  // 댓글 등록
+			commentGroup.GET("/:id", handler.CommentHandler.GetComment)                   // 댓글 단일 조회
+			commentGroup.GET("/wish/:wishID", handler.CommentHandler.GetCommentsByWishID) // 특정 소원의 댓글 조회
+			commentGroup.DELETE("/:id", handler.CommentHandler.DeleteComment)             // 댓글 삭제
 		}
 	}
 	return router
