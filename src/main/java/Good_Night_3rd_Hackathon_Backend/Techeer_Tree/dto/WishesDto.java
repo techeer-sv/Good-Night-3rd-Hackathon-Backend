1868dbac437a356eefa1ddc36752c2091ec14a6b
kimzini <rlawlsdl11111@gmail.com>
2024-08-25 01:27:08 +0900

    feat : WishesDto 구현

--- src/main/java/Good_Night_3rd_Hackathon_Backend/Techeer_Tree/dto/WishesDto.java
+++ src/main/java/Good_Night_3rd_Hackathon_Backend/Techeer_Tree/dto/WishesDto.java
@@ -1,5 +1,18 @@
 package Good_Night_3rd_Hackathon_Backend.Techeer_Tree.dto;
 
-public class WishesDto {
+import Good_Night_3rd_Hackathon_Backend.Techeer_Tree.domain.Wishes.Category;
+import Good_Night_3rd_Hackathon_Backend.Techeer_Tree.domain.Wishes.WishesStatus;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class WishesDto {
+    private String title;
+    private String content;
+    private Category category;
+    private LocalDateTime createdAt;
+    private WishesStatus isConfirm;
 }
