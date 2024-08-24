f8fab2b04b03c4309bf6a7ec89a2f76b2d5a7841
sanghun <vitash1215@gmail.com>
2024-08-24 18:07:14 +0900

    DB 연결, Swagger 세팅 등 설정 추가

---
+++ src/main/java/goodnight/tree/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package goodnight.tree.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 접속 주소 : http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("test")
+                .description("API Docs")
+                .version("1.0.0");
+    }
+}
