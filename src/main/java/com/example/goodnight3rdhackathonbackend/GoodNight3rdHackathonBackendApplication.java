6c7e2c2974fe31442ed2018ebce83c6ac225bf65
Bobae Kim <rlaqhqo0418@naver.com>
2024-08-24 12:44:57 +0900

    feat: spring project 생성

---
+++ src/main/java/com/example/goodnight3rdhackathonbackend/GoodNight3rdHackathonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodnight3rdhackathonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodNight3rdHackathonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodNight3rdHackathonBackendApplication.class, args);
+	}
+
+}
