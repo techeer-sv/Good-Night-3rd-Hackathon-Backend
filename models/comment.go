b224717318a2de99e657aca460ad93701b00eb29
Jaehoon <gkdwngo@gmail.com>
2024-08-25 03:37:38 +0900

    Comment 모델 정의

---
+++ models/comment.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	ID        int            `json:"id" gorm:"primaryKey"`
+	WishID    uint            `json:"wish_id"`
+	Wish      Wish           `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`  // 외래 키 설정
+	Content   string         `json:"content" validate:"required"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
