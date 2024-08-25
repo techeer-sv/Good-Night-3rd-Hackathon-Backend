15695f4779c26cf033aa547ffda3c3d43abece3d
dongmin <dongmin11566@gmail.com>
2024-08-25 20:11:14 +0900

    feat: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // 기본 CORS 설정
+  app.enableCors();
+
+  await app.listen(8080);
 }
 bootstrap();
