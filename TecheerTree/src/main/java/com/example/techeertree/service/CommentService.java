c70ea5892063dfe8697a956363b4d0d7d0ab1f44
82everywin <82everywin@gmail.com>
2024-08-25 22:27:58 +0900

    feat: 댓글 등록 (승인된 소원에서만) 기능 구현

--- TecheerTree/src/main/java/com/example/techeertree/service/CommentService.java
+++ TecheerTree/src/main/java/com/example/techeertree/service/CommentService.java
@@ -1,10 +1,12 @@
 package com.example.techeertree.service;
 
 import com.example.techeertree.domain.Comment;
+import com.example.techeertree.domain.Confirm;
 import com.example.techeertree.domain.Wish;
 import com.example.techeertree.dto.comment.CommentMapper.*;
 import com.example.techeertree.dto.comment.CommentRequestDto.*;
 import com.example.techeertree.dto.comment.CommentResponseDto.*;
+import com.example.techeertree.dto.wish.WishResponseDto;
 import com.example.techeertree.exception.BaseException;
 import com.example.techeertree.exception.ErrorCode;
 import com.example.techeertree.repository.CommentRepository;
@@ -13,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +22,21 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final WishRepository wishRepository;
 
-    
+
     public CommentInfoResponseDto create(Long id, CommentCreateRequestDto commentCreateRequestDto) {
         // 소원 존재 유무 조회
-        Wish wish = wishRepository.findById(id).orElseThrow(() -> new BaseException(ErrorCode.NOT_EXIST_WISH));
+        Wish wish = wishRepository.findById(id)
+                        .filter(w -> {
+                            if (w.getIsConfirm() != Confirm.CONFIRM) {
+                                throw new BaseException(ErrorCode.NOT_CONFIRMED);
+                            }
+                            return true;
+                        })
+                        .orElseThrow(() -> new BaseException(ErrorCode.NOT_EXIST_WISH));
 
         // 댓글 객체 생성
         Comment comment = CommentCreateMapper.INSTANCE.toEntity(commentCreateRequestDto);
-        comment.setWish(wish);
+        comment.matchWish(wish);
         commentRepository.save(comment);
 
         // 소원에 댓글 등록
@@ -39,4 +47,5 @@ public class CommentService {
     }
 
 
+    
 }
